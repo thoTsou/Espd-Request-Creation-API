9b25f8bbb3c490b3b52ba1a95d2c4be42cee9450
thoTsou <tsoufis.thodoris@gmail.com>
2021-03-25 23:01:27 +0200

    minor changes in api security

--- src/main/java/com/espd/builder/WebSecurityConfig.java
+++ src/main/java/com/espd/builder/WebSecurityConfig.java
@@ -27,7 +27,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.httpBasic()
+                .and().
+                authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
